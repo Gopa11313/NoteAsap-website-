52d4ae96d2635eb0be23b9a4a65b9723086f0724
gopa11313 <gopalaa9819525993@gmail.com>
2021-04-01 22:08:16 +0545

    import tean design in Bookmarked page

--- frontend/src/Body/Bookmarked.js
+++ frontend/src/Body/Bookmarked.js
@@ -5,6 +5,8 @@ import { Card, Button } from 'react-bootstrap'
 import notes from '../assets/notes.png'
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
+import Team from '../Body/Team'
+import Footer from '../Footer/Footer'
 const Styles = styled.div`
   .Background = styled.div({
     backgroundSize: "cover",
@@ -23,12 +25,12 @@ export class Bookmarked extends Component {
         config: {
             headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` }
         },
-        id:localStorage.getItem('id')
+        id: localStorage.getItem('id')
 
     }
 
     componentDidMount() {
-        axios.get("http://localhost:90/bookmark/notes/"+this.state.id,this.state.config)
+        axios.get("http://localhost:90/bookmark/notes/" + this.state.id, this.state.config)
             .then((response) => {
                 this.setState({
                     notes: response.data.data
@@ -37,10 +39,10 @@ export class Bookmarked extends Component {
             .catch((err) => {
                 console.log(err.response)
             })
-            
+
     }
-    deletenotes=(ab)=>{
-        Axios.delete("http://localhost:90/delete/bookmark/" +ab, this.state.config)
+    deletenotes = (ab) => {
+        Axios.delete("http://localhost:90/delete/bookmark/" + ab, this.state.config)
             .then((response) => {
                 console.log(response.data.data)
                 window.location.href = "/bookmark";
@@ -51,38 +53,42 @@ export class Bookmarked extends Component {
     }
     render() {
         return (
-            <div style={{}}>
-            <div className="container">
-                <div className="row">
-                    {
-                        this.state.notes.map((c) => {
-                            return (
-                                <div className="col-md-4" style={{padding:50}}>
-                                    <Styles>
-                                        <Card style={{ width: '20rem',padding:10}}>
-                                            <Card.Img variant="top" src={notes} />
-                                            <Card.Body>
-                                                <Card.Title>{c.topic}</Card.Title>
-                                                <Card.Text>
-                                                    {c.description}
-                                                </Card.Text>
-                                                {/* <Button variant="primary">Open</Button> */}
-                                                <Button variant="primary"onClick={this.deletenotes.bind(this,c.noteId)}>Remove Bookmarked</Button>
-                                            </Card.Body>
-                                            <Card.Footer>
-                                                <small className="text-muted">Last updated 3 mins ago</small>
-                                            </Card.Footer>
-                                        </Card>
-                                    </Styles>
-                                </div>
-                            )
-                        })
-                    }
+            <>
+                <div style={{}}>
+                    <div className="container">
+                        <div className="row">
+                            {
+                                this.state.notes.map((c) => {
+                                    return (
+                                        <div className="col-md-4" style={{ padding: 50 }}>
+                                            <Styles>
+                                                <Card style={{ width: '20rem', padding: 10 }}>
+                                                    <Card.Img variant="top" src={notes} />
+                                                    <Card.Body>
+                                                        <Card.Title>{c.topic}</Card.Title>
+                                                        <Card.Text>
+                                                            {c.description}
+                                                        </Card.Text>
+                                                        {/* <Button variant="primary">Open</Button> */}
+                                                        <Button variant="primary" onClick={this.deletenotes.bind(this, c.noteId)}>Remove Bookmarked</Button>
+                                                    </Card.Body>
+                                                    <Card.Footer>
+                                                        <small className="text-muted">Last updated 3 mins ago</small>
+                                                    </Card.Footer>
+                                                </Card>
+                                            </Styles>
+                                        </div>
+                                    )
+                                })
+                            }
 
-                    { }
+                            { }
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
+                <Team/>
+                <Footer/>
+            </>
         )
     }
 }
