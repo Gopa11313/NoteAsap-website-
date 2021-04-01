84465244b9e4f0f4e1c0a245865d06c09a05e1d5
gopa11313 <gopalaa9819525993@gmail.com>
2021-04-01 21:11:42 +0545

    trying to build team section

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Carousel, Container } from 'react-bootstrap'
 import Footer from '../Footer/Footer'
 import books from '../assets/books.png'
+import note2 from '../assets/notes2.jpg'
+import note3 from '../assets/notes3.jpg'
 export const About = () => (
     <>
-        <Container>
+        <Container className="mb-4">
             <div className="row featurette" style={{ display: "flex" }}>
                 <div className="col-md-7">
                     <h2 className="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
@@ -21,7 +23,7 @@ export const About = () => (
                     <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                 </div>
                 <div className="col-md-5 order-md-1">
-                    <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x450" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17886a9a818%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17886a9a818%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.125%22%20y%3D%22261.2828125%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style={{ "width": "500px", height: "450px;" }} />
+                    <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x450"  src={note2} data-holder-rendered="true" style={{ "width": "500px", height: "450px;" }} />
                 </div>
             </div>
 
@@ -32,7 +34,7 @@ export const About = () => (
                     <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                 </div>
                 <div className="col-md-5">
-                    <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x450" style={{ "width": "500px", height: "450px" }} src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17886a9a815%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17886a9a815%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.125%22%20y%3D%22261.2828125%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" />
+                    <img className="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x450" style={{ "width": "500px", height: "450px" }} src={note3} data-holder-rendered="true" />
                 </div>
             </div>
 
