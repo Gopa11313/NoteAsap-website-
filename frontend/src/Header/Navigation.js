e7b64df92f804e92bb174469e51213f6c9dfe389
gopa11313 <gopalaa9819525993@gmail.com>
2021-04-01 22:26:04 +0545

    done displaying image on nav

--- frontend/src/Header/Navigation.js
+++ frontend/src/Header/Navigation.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
 import { Link } from 'react-router-dom'
+import Axios from "axios"
 import { Navbar, FormControl, Button, Form, NavDropdown, Nav } from 'react-bootstrap'
 import { Search } from 'react-bootstrap-icons';
 const Styles = styled.div`
@@ -21,6 +22,41 @@ export class Navigation extends Component {
         localStorage.removeItem('token')
         localStorage.removeItem('id')
         window.location.href = "/";
+    }
+    state = {
+        "name1": "",
+        "email": "",
+        file: "",
+        config: {
+            headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` }
+        },
+        userId: localStorage.getItem('id')
+
+    }
+    inputHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    fileHandler = (e) => {
+        this.setState({
+            file: e.target.files[0]
+        })
+    }
+    componentDidMount() {
+        Axios.get("http://localhost:90/get/me/web/" + this.state.userId, this.state.config)
+            .then((response) => {
+                console.log(response.data.data)
+                this.setState({
+                    name1: response.data.data.name,
+                    email: response.data.data.email,
+                    file: response.data.data.image
+                })
+                console.log(this.state)
+            })
+            .catch((err) => {
+                console.log(err.response)
+            })
     }
     render() {
         if(localStorage.getItem('token')){
@@ -67,6 +103,9 @@ export class Navigation extends Component {
                     </Nav.Item>
                 </Nav>
                 <Form inline>
+                <img variant="outline-success" className="rounded-circle mr-2" src={"http://localhost:90/images/"
+                            +this.state.file}
+                                width="40px" height="40px" ></img>
                     <Button className="ml-1 bg-dark text-light" onClick={this.removetoken}>logOut</Button>
                 </Form>
             </Navbar.Collapse>
