bd4ebc7c997a55349469e5750e0985b9c2815934
gopa11313 <gopalaa9819525993@gmail.com>
2021-04-05 10:02:50 +0545

    done bookmarking the note

--- frontend/src/Body/Display.js
+++ frontend/src/Body/Display.js
@@ -22,7 +22,9 @@ class Display extends Component {
         notes: [],
         config: {
             headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` }
-        }
+        },
+        userId: localStorage.getItem('id'),
+        noteId: ""
 
     }
     componentDidMount() {
@@ -36,40 +38,58 @@ class Display extends Component {
                 console.log(err.response)
             })
     }
+    Bookmark = (ab) => {
+        if (localStorage.getItem('token')) {
+            const data = new FormData() // new line
+            data.append('noteId', ab)
+            data.append('userId', localStorage.getItem('id'))
+            axios.post("http://localhost:90/note/bookmark", data, this.state.config)
+                .then((response) => {
+                    alert("you have successfully bookmarked this note")
+                    window.location.href = "/";
+                })
+                .catch((err) => {
+                    console.log(err.response)
+                })
+        }
+        else {
+            alert("To Bookmark Noe=te you have to log in.")
+        }
+    }
     render() {
         return (
             <div style={{}}>
-            <div className="container">
-                <div className="row">
-                    {
-                        this.state.notes.map((b) => {
-                            return (
-                                <div className="col-md-4" style={{padding:50}}>
-                                    <Styles>
-                                        <Card style={{ width: '20rem',padding:10}}>
-                                            <Card.Img variant="top" src={notes} />
-                                            <Card.Body>
-                                                <Card.Title>{b.topic}</Card.Title>
-                                                <Card.Text>
-                                                    {b.description}
-                                                </Card.Text>
-                                                <Button variant="primary">Open</Button>
-                                                {/* <p variant="primary" ><Link to={'/updateNote/'+b._id}>Update</Link> </p> */}
-                                                
-                                            </Card.Body>
-                                            <Card.Footer>
-                                                <small className="text-muted">Last updated 3 mins ago</small>
-                                            </Card.Footer>
-                                        </Card>
-                                    </Styles>
-                                </div>
-                            )
-                        })
-                    }
+                <div className="container">
+                    <div className="row">
+                        {
+                            this.state.notes.map((b) => {
+                                return (
+                                    <div className="col-md-4" style={{ padding: 50 }}>
+                                        <Styles>
+                                            <Card style={{ width: '20rem', padding: 10 }}>
+                                                <Card.Img variant="top" src={notes} />
+                                                <Card.Body>
+                                                    <Card.Title>{b.topic}</Card.Title>
+                                                    <Card.Text>
+                                                        {b.description}
+                                                    </Card.Text>
+                                                    <Button variant="primary" onClick={this.Bookmark.bind(b._id)}>Bookmark</Button>
+                                                    {/* <p variant="primary" ><Link to={'/updateNote/'+b._id}>Update</Link> </p> */}
 
-                    { }
+                                                </Card.Body>
+                                                <Card.Footer>
+                                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                                </Card.Footer>
+                                            </Card>
+                                        </Styles>
+                                    </div>
+                                )
+                            })
+                        }
+
+                        { }
+                    </div>
                 </div>
-            </div>
             </div>
         )
     }
